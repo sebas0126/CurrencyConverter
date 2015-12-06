6a6eff89cb81c9301dd993c84eaf49cc91be6463
sebas0126 <sebas0126@gmail.com>
2015-12-06 12:11:19 -0500

    Paquetes agregados

--- app/src/main/java/com/anjuse/test/currencyconverter/view/MainActivity.java
+++ app/src/main/java/com/anjuse/test/currencyconverter/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.anjuse.test.currencyconverter;
+package com.anjuse.test.currencyconverter.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.anjuse.test.currencyconverter.R;
 
 public class MainActivity extends AppCompatActivity {
 
