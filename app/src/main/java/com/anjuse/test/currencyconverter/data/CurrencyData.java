80dbd1229041daeb61552e86b7b53ddf0ee54348
anamariavelca <anamariavelca@hotmail.com>
2015-12-07 11:23:25 -0500

    Falta multiplicar lo ingresado por el usuario por lo retornado por el servicio y desplegarlo

--- app/src/main/java/com/anjuse/test/currencyconverter/data/CurrencyData.java
+++ app/src/main/java/com/anjuse/test/currencyconverter/data/CurrencyData.java
@@ -1,13 +1,12 @@
 package com.anjuse.test.currencyconverter.data;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.anjuse.test.currencyconverter.R;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
@@ -15,12 +14,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.concurrent.CountDownLatch;
 
 public class CurrencyData {
 
-    private String charset="UTF-8";
     private JSONObject JSONresponse = null;
     private CountDownLatch reqLatch;
     private String LOG_TAG = CurrencyData.class.getSimpleName();
@@ -33,7 +30,6 @@ public class CurrencyData {
         req.execute(url, base); //Aca hay que mandar los parametros que se recibieron (url y base) y recibir un JSONObject como respuesta
         try {
             reqLatch.await();
-            Log.v("reqLatch", "doInBackground BEFORE " + reqLatch.getCount());
             return JSONresponse.getJSONObject("rates"); //Aca hay que retornar el JSONObject de los current del JSONObject que se recibio arriba
         } catch (InterruptedException | JSONException e) {
             Log.e(LOG_TAG, e.getMessage());
@@ -44,7 +40,6 @@ public class CurrencyData {
 
 
     private void urlConnection(String url, String parameters){
-        URLConnection connection = null;
         try {
             InputStream response = new URL(url + "?" + parameters).openStream();
             BufferedReader streamReader = new BufferedReader(new InputStreamReader(response, "UTF-8"));
@@ -53,28 +48,13 @@ public class CurrencyData {
             while ((inputStr = streamReader.readLine()) != null)
                 responseStrBuilder.append(inputStr);
             JSONresponse = new JSONObject(responseStrBuilder.toString());
-            //reqLatch.countDown();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
+        } catch (JSONException | IOException e) {
+            Log.e(LOG_TAG, e.getMessage());
             e.printStackTrace();
         }
     }
 
     private class GetRequest extends AsyncTask<String, Void, Boolean> {
-        ProgressDialog pDialog;
-        public GetRequest(){
-            pDialog = new ProgressDialog(appContext);
-            pDialog.setMessage(appContext.getResources().getString(R.string.dialog_message));
-            pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            pDialog.setCancelable(false);
-        }
-
-        protected void onPreExecute() {
-            pDialog.show();
-            Log.v("PRE", "onPreExecute");
-        }
-
         //args[0] url
         //args[1] paramétro
         protected Boolean doInBackground(String... params) {
@@ -89,9 +69,8 @@ public class CurrencyData {
         }
 
         protected void onPostExecute(Boolean success) {
-            Log.v("POST", "onPostExecute BEFORE " + success);
-            pDialog.dismiss();
-            Log.v("POST", "onPostExecute AFTER");
+            if(!success)
+                Toast.makeText(appContext, appContext.getResources().getString(R.string.request_error_msg), Toast.LENGTH_SHORT).show();
         }
     }
 
